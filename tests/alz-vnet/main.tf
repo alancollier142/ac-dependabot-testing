c8cfec4e01c838c3c3ec088150ee8f7e9ccda6d4
alancollier142 <alan.collier@justice.gov.uk>
2024-10-23 09:09:06 +0100

    test format-code and tf-static-analysis actions

--- tests/alz-vnet/main.tf
+++ tests/alz-vnet/main.tf
@@ -1,6 +1,6 @@
 # Test module
 module "testrg" {
-  source      = "../../modules/alz-vnet"
-  rg_name     = "rg-ac-testing-dependabot"
-  rg_location = "uksouth"
+  source           = "../../modules/alz-vnet"
+  rg_name      = "rg-ac-testing-dependabot"
+  rg_location      = "uksouth"
 }
